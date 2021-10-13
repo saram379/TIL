19c4ef00f74194adad99e3707b1ff1a5238610a6
SARAM <saramtls379@gmail.com>
2021-10-13 21:46:47 +0900

    sorry

--- highschool/math/matrix_retry/inware.js
+++ highschool/math/matrix_retry/inware.js
@@ -6,3 +6,9 @@ const printMatrixB = () => {
     let matrix = document.getElementsByClassName('inputmatrix')[1].value;
     document.getElementById('resultB').innerText = matrix;
 }
+const printMatrix = () => {
+    let Arow = document.getElementById('Arow').value;
+    let Acol = document.getElementById('Acol').value;
+    let Brow = document.getElementById('Brow').value;
+    let Bcol = document.getElementById('Bcol').value;
+}
